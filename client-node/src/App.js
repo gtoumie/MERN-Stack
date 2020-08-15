4325d470b7a8ce9bc0f024d119b8096427e06963
Ghassan T <gtoumie@gmail.com>
2020-08-16 01:00:20 +0300

    adding again

---
+++ client-node/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import AppNavbar from './components/AppNavbar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+      <AppNavbar ></AppNavbar>
+    </div>
+  );
+}
+
+export default App;
