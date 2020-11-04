05a8c6f1d2fa5bf8297990a0f8abbdf4030daa34
Ghassan T <gtoumie@gmail.com>
2020-11-04 23:41:36 +0200

    version 0.0.1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const items = require('./routes/api/items');
 
 const app = express();
@@ -18,6 +18,13 @@ mongoose.connect(db)
 // Use Routes
 app.use('/api/items', items);
 
+if (process.env.NODE_ENV==='production'){
+    app.use(express.static('client-node/build'))
+    
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'client-node', 'build', 'index.html'))
+    });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>console.log(`Server started on port ${port}`));
